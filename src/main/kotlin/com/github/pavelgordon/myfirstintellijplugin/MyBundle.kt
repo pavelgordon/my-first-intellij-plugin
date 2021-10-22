f0f6ff8c450326a1cfe98f4fa85b35c2ae614049
GitHub Action <action@github.com>
2021-10-22 07:18:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/pavelgordon/myfirstintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/pavelgordon/myfirstintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pavelgordon.myfirstintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/pavelgordon/myfirstintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pavelgordon.myfirstintellijplugin.services
+
+import com.github.pavelgordon.myfirstintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
